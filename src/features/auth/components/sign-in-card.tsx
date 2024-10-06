f5eed01ee1b775d69c7ad26c149fec7a443aac94
suqingyao <suqingyao333@gmail.com>
2024-10-06 21:43:31 +0800

    style: fix card style

--- src/features/auth/components/sign-in-card.tsx
+++ src/features/auth/components/sign-in-card.tsx
@@ -27,8 +27,8 @@ export const SignInCard = ({ setState }: SignInProps) => {
     <Card className="w-full h-full p-8">
       <CardHeader className="px-0 pt-0">
         <CardTitle>Login to continue</CardTitle>
+        <CardDescription>Use your email or another service to continue</CardDescription>
       </CardHeader>
-      <CardDescription>Use your email or another service to continue</CardDescription>
       <CardContent className="space-y-5 px-0 pb-0">
         <form className="space-y-2.5">
           <Input
--- src/features/auth/components/sign-up-card.tsx
+++ src/features/auth/components/sign-up-card.tsx
@@ -21,10 +21,10 @@ export const SignUpCard = ({ setState }: SignUpProps) => {
     <Card className="w-full h-full p-8">
       <CardHeader className="px-0 pt-0">
         <CardTitle>Sign up to continue</CardTitle>
+        <CardDescription>Use your email or another service to continue</CardDescription>
       </CardHeader>
-      <CardDescription>Use your email or another service to continue</CardDescription>
       <CardContent className="space-y-5 px-0 pb-0">
-        <form className="space-y-2.5">
+        <form className="flex flex-col gap-2 space-y-2.5">
           <Input
             disabled={false}
             value={email}
