cd5eeb71e1996cc3eab21f8df363740b4a86cc91
suqingyao <suqingyao333@gmail.com>
2024-11-08 23:37:00 +0800

    feat: emoji picker component

--- src/components/editor.tsx
+++ src/components/editor.tsx
@@ -6,9 +6,12 @@ import { PiTextAa } from 'react-icons/pi';
 import { MdSend } from 'react-icons/md';
 
 import 'quill/dist/quill.snow.css';
+
+import { cn } from '@/lib/utils';
+
 import { Button } from './ui/button';
 import { Hint } from './hint';
-import { cn } from '@/lib/utils';
+import { EmojiPopover } from './emoji-popover';
 
 type EditorValue = {
   image: File | null;
@@ -127,6 +130,12 @@ const Editor = ({
     }
   };
 
+  const onEmojiSelect = (emoji: any) => {
+    const quill = quillRef.current;
+
+    quill?.insertText(quill.getSelection()?.index || 0, emoji.native);
+  };
+
   const isEmpty = text.replace(/<(.|\n)*?>/g, '').trim().length === 0;
 
   return (
@@ -147,16 +156,15 @@ const Editor = ({
               <PiTextAa className="size-4" />
             </Button>
           </Hint>
-          <Hint label="Emoji">
+          <EmojiPopover onEmojiSelect={onEmojiSelect}>
             <Button
               disabled={disabled}
               size="iconSm"
               variant="ghost"
-              onClick={() => {}}
             >
               <Smile className="size-4" />
             </Button>
-          </Hint>
+          </EmojiPopover>
           {variant === 'create' && (
             <Hint label="Image">
               <Button
@@ -205,11 +213,18 @@ const Editor = ({
           )}
         </div>
       </div>
-      <div className="p-2 text-[10px] text-muted-foreground flex justify-end">
-        <p>
-          <strong>Shift + Return</strong> to add new line
-        </p>
-      </div>
+      {variant === 'create' && (
+        <div
+          className={cn(
+            'p-2 text-[10px] text-muted-foreground flex justify-end opacity-0 transition',
+            !isEmpty && 'opacity-100',
+          )}
+        >
+          <p>
+            <strong>Shift + Return</strong> to add new line
+          </p>
+        </div>
+      )}
     </div>
   );
 };
