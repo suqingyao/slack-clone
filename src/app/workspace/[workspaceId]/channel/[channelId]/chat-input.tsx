867a62d9f9b10aaa8eb3bf8341adc9134d65c1f1
suqingyao <suqingyao333@gmail.com>
2024-11-09 09:32:32 +0800

    feat: image upload

--- src/app/workspace/[workspaceId]/channel/[channelId]/chat-input.tsx
+++ src/app/workspace/[workspaceId]/channel/[channelId]/chat-input.tsx
@@ -5,15 +5,26 @@ import { toast } from 'sonner';
 
 import { type EditorValue } from '@/components/editor';
 
-import { useCreateMessage } from '@/features/messages/api/use-create-message';
 import { useWorkspaceId } from '@/hooks/use-workspace-id';
 import { useChannelId } from '@/hooks/use-channel-id';
+
+import { useCreateMessage } from '@/features/messages/api/use-create-message';
+import { useGenerateUploadUrl } from '@/features/upload/api/use-generate-upload-url';
+
+import { Id } from '../../../../../../convex/_generated/dataModel';
 
 const Editor = dynamic(() => import('@/components/editor'), { ssr: false });
 
 interface ChatInputProps {
   placeholder: string;
 }
+
+type CreateMessageValues = {
+  channelId: Id<'channels'>;
+  workspaceId: Id<'workspaces'>;
+  body: string;
+  image: Id<'_storage'> | undefined;
+};
 
 export const ChatInput = ({ placeholder }: ChatInputProps) => {
   const [editorKey, setEditorKey] = useState(0);
@@ -23,21 +34,49 @@ export const ChatInput = ({ placeholder }: ChatInputProps) => {
 
   const workspaceId = useWorkspaceId();
   const channelId = useChannelId();
+
   const { mutate: createMessage } = useCreateMessage();
+  const { mutate: generateUploadUrl } = useGenerateUploadUrl();
 
   const handleSubmit = async ({ body, image }: EditorValue) => {
     try {
       setIsPending(true);
-      await createMessage(
-        {
-          workspaceId,
-          channelId,
-          body,
-        },
-        {
-          throwError: true,
-        },
-      );
+      editorRef.current?.enable(false);
+
+      const values: CreateMessageValues = {
+        channelId,
+        workspaceId,
+        body,
+        image: undefined,
+      };
+
+      if (image) {
+        const url = await generateUploadUrl({}, { throwError: true });
+
+        if (!url) {
+          throw new Error('Url not found');
+        }
+
+        const result = await fetch(url, {
+          method: 'POST',
+          headers: {
+            'Content-Type': image.type,
+          },
+          body: image,
+        });
+
+        if (!result.ok) {
+          throw new Error('Failed to upload image');
+        }
+
+        const { storageId } = await result.json();
+
+        values.image = storageId;
+      }
+
+      await createMessage(values, {
+        throwError: true,
+      });
       // rerender editor for clear editor content
       setEditorKey((prevKey) => prevKey + 1);
       // editorRef.current?.setContents([]);
@@ -45,6 +84,7 @@ export const ChatInput = ({ placeholder }: ChatInputProps) => {
       toast.error('Failed to send message');
     } finally {
       setIsPending(false);
+      editorRef.current?.enable(true);
     }
   };
 
--- src/features/upload/api/use-generate-upload-url.ts
+++ src/features/upload/api/use-generate-upload-url.ts
@@ -1,17 +1,8 @@
 import { useCallback, useMemo, useState } from 'react';
 import { useMutation } from 'convex/react';
 import { api } from '../../../../convex/_generated/api';
-import { Id } from '../../../../convex/_generated/dataModel';
 
-type RequestType = {
-  body: string;
-  image?: Id<'_storage'>;
-  workspaceId: Id<'workspaces'>;
-  channelId: Id<'channels'>;
-  parentMessageId?: Id<'messages'>;
-};
-
-type ResponseType = Id<'messages'> | null;
+type ResponseType = string | null;
 
 type Options = {
   onSuccess?: (data: ResponseType) => void;
@@ -20,7 +11,7 @@ type Options = {
   throwError?: boolean;
 };
 
-export const useCreateMessage = () => {
+export const useGenerateUploadUrl = () => {
   const [data, setData] = useState<ResponseType>(null);
   const [error, setError] = useState<Error | null>(null);
   const [status, setStatus] = useState<'success' | 'error' | 'settled' | 'pending' | null>(null);
@@ -30,17 +21,17 @@ export const useCreateMessage = () => {
   const isError = useMemo(() => status === 'error', [status]);
   const isSettled = useMemo(() => status === 'settled', [status]);
 
-  const mutation = useMutation(api.messages.create);
+  const mutation = useMutation(api.upload.generateUploadUrl);
 
   const mutate = useCallback(
-    async (values: RequestType, options?: Options) => {
+    async (_values: {}, options?: Options) => {
       try {
         setData(null);
         setError(null);
 
         setStatus('pending');
 
-        const response = await mutation(values);
+        const response = await mutation();
         setData(response);
         options?.onSuccess?.(response);
         return response;
